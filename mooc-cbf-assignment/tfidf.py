c6d5c1c59fc0a4cd7ce0d22e41b4cee9cff25667
ioannis.mermigkis <ioannis.mermigkis@siemens-enterprise.com>
2013-09-30 11:26:39 +0300

    TFIDF python start

--- mooc-cbf-assignment/tfidf.py
+++ mooc-cbf-assignment/tfidf.py
@@ -1,24 +1,66 @@
+from math import log2
+
 tags_file=open('data/movie-tags.csv')
 titles_file=open('data/movie-titles.csv')
-ratings_file=open('data/ratings.csv',errors='ignore')
+ratings_file=open('data/ratings.csv')
 user_file=open('data/users.csv')
 
-#tags={ (row.split(',')[1],row.split(',')[0]) : (tags[(row.split(',')[1],row.split(',')[0])]+1 if (row.split(',')[1],row.split(',')[0]) in tags else 1) for row in list(tags_file)}
 
-tags=dict()
+tags_movies=dict()
+movies_tag=dict()
+
+def get_rows(file):
+    for row in file:
+        rows = row.split('#')
+        for r in rows:
+            if ',' in r:
+                yield r
+
+
+
+for row in get_rows(tags_file):
+    r=row.split(',')
+    tag = r[1]
+    movieId = int(r[0])
+    if movieId in movies_tag:
+        if tag in movies_tag[movieId]:
+            movies_tag[movieId][tag]+=1
+        else:
+            movies_tag[movieId][tag] = 1
+    else:
+        movies_tag[movieId] = {tag:1}
+        
+    if tag in tags_movies:
+        if movieId in tags_movies[tag]:
+            tags_movies[tag][movieId]+=1
+        else:
+            tags_movies[tag][movieId]=1
+    else:
+        tags_movies[tag] = {movieId:1}
+
+print(tags_movies.keys())
+
+user_ratings=dict()
+movie_rating = dict()
+
 for row in ratings_file:
     r=row.split(',')
-    if r[1] in tags:
-        d_t = tags[r[1]]
-        if r[0] in d_t:
-            d_t[r[0]]+=1
-        else:
-            d_t[r[0]] = 1
-        tags[r[1]]= d_t
+    userId=int(r[0])
+    movieId=int(r[1])
+    rating = float(r[2])
+    if userId in user_ratings:
+        user_ratings[userId][movieId] = rating
     else:
-        tags[r[1]] = {r[0]:1}
+        user_ratings[userId]={movieId:rating}  
+    
+    if movieId in movie_rating:
+        if rating in movie_rating[movieId]:
+            movie_rating[movieId][rating] +=1
+        else:
+            movie_rating[movieId][rating] = 1
+    else:
+        movie_rating[movieId] = {rating:1}
 
-print(tags)
 
 
 
@@ -27,3 +69,4 @@ print(tags)
                                            
 
     
+ 
